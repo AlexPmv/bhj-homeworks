3e521266ca3348fd86db1d6c98cad44dcf023855
Alex Palmov (PC) <likeitsweet@mail.ru>
2022-07-13 12:19:28 +0300

    dom tasks edited

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,10 +1,22 @@
-const fontControlBlock = document.querySelector('.book__control_font-size');
-const fontControlLinks = Array.from(document.querySelector('.book__control_font-size').querySelectorAll('.font-size'));
-const content = document.querySelector('.book__content');
+const sizeControlBlock = document.querySelector('.book__control_font-size');
+const colorControlBlock = document.querySelector('.book__control_color');
+const backgroundControlBlock = document.querySelector('.book__control_background');
 
-fontControlBlock.addEventListener('click', function(event) {
-    fontControlLinks.find((link) => link.classList.contains('font-size_active')).classList.toggle('font-size_active');
-    event.target.classList.toggle('font-size_active');
+const sizeControlButtons = Array.from(sizeControlBlock.querySelectorAll('a.font-size'));
+const colorControlButtons = Array.from(colorControlBlock.querySelectorAll('a.color'));
+const backgroundControlButtons = Array.from(backgroundControlBlock.querySelectorAll('a.color'));
+
+const content = document.querySelector('.book__content');
+const book = document.querySelector('.book');
+
+sizeControlBlock.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (event.target.tagName === 'A') {
+        sizeControlButtons.find((link) => link.classList.contains('font-size_active')).classList.toggle('font-size_active');
+        event.target.classList.toggle('font-size_active');
+    };  
+
     content.className = 'book__content';
     
     if (!event.target.dataset.size) {
@@ -12,6 +24,24 @@ fontControlBlock.addEventListener('click', function(event) {
     };
 
     content.classList.add(`book_fs-${event.target.dataset.size}`);
+});
 
+colorControlBlock.addEventListener('click', function(event) {
     event.preventDefault();
+
+    if (event.target.tagName === 'A') {
+        colorControlButtons.find((link) => link.classList.contains('color_active')).classList.toggle('color_active');
+        event.target.classList.toggle('color_active');
+        content.style.color = event.target.dataset.textColor;
+    };
+});
+
+backgroundControlBlock.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (event.target.tagName === 'A') {
+        backgroundControlButtons.find((link) => link.classList.contains('color_active')).classList.toggle('color_active');
+        event.target.classList.toggle('color_active');
+        book.style.background = event.target.dataset.bgColor;
+    };
 });
